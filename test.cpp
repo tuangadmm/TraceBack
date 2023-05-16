ba89ff5789124d25cffcdbd6be785a3436513a68
tuangadmm <42936087+tuangadmm@users.noreply.github.com>
2023-05-16 16:34:03 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,18 +1,12 @@
-#include <iostream>
 #include "TraceBack.h"
 
-
-
 int main() {
-
+	//this test check for Plants vs. Zombies Steam GOTY  Edition Sun address
+	//base bodule address = 0x0400000
 	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, 1824);
-	int offset[2] = { 0x331C50 , 0x868 };
-	int		size = sizeof(offset) / sizeof(offset[0]);		//offsets array size
+	int offset[2] = { 0x331C50 , 0x868, 0x5578 };
+	int		size = sizeof(offset) / sizeof(offset[0]);
 
 	std::cout << std::hex << std::uppercase << "0x0" << DereferencePointer(hProc, (void*)0x0400000, offset, size);
-
-
-
-
 	std::cin.get();
 }
